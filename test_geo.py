60fa1e01d0427bce974bee9885b80ba64fda6b9f
Amy Ponter <acp89@cam.ac.uk>
2022-02-07 12:17:49 +0000

    test for 1D

--- test_geo.py
+++ test_geo.py
@@ -5,6 +5,8 @@ from . import datafetcher
 from floodsystem.station import MonitoringStation
 from floodsystem.geo import stations_by_distance
 from floodsystem.geo import stations_within_radius
+from floodsystem.geo import rivers_with_station
+from floodsystem.geo import stations_by_river
 
 def test_stations_by_distance():
     stations = build_station_list()
@@ -17,3 +19,16 @@ def test_stations_within_radius():
     stations = build_station_list()
     station_distances = stations_within_radius(stations, (52.2053, 0.1218), 10)
     assert isinstance(station_distances[0], MonitoringStation)
+
+def test_rivers_with_station():
+    stations = build_station_list()
+    river_test = rivers_with_station(stations)
+    assert "River Aire" in river_test
+
+def test_stations_by_river():
+    stations = build_station_list()
+    river_test = stations_by_river(stations)
+    assert "River Aire" in river_test
+
+
+
