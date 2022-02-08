5f80fb0e2eea114e34325f56a7b8746d29fb4b2f
Reve Kragbe <rk681@cam.ac.uk>
2022-02-08 10:46:11 +0000

    commit E

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -67,10 +67,34 @@ def stations_by_river(stations):
 
     return (rivers_dict)
 
+#E
+def rivers_by_station_number(stations, N):
+    rivers = []
+    for station in stations:
+        rivers = rivers + [station.river]
+    rivers = set(rivers)
+    
+    #list of stations for every river
+    rivers_with_station = []
+    for river in rivers:
+        station_num = 0
+        for station in stations:
+            if station.river == river:
+                station_num += 1
+        rivers_with_station.append((river,station_num))
 
+    sorted_rivers_with_stations = sorted(rivers_with_station, reverse = True, key= lambda i:i[1] )
 
+    largest = sorted_rivers_with_stations[:N]
 
-
+    for river in sorted_rivers_with_stations[N:]:
+        nth_river = largest[N-1]
+        if river[1] == nth_river[1]:
+            largest += [river]
+    
+        
+    print len(largest)
+    return largest
 
 
 
---
+++ task1E.py
@@ -0,0 +1,13 @@
+from re import S
+from floodsystem import geo
+from floodsystem.stationdata import build_station_list
+
+
+def run():
+    N = 9
+    stations = build_station_list()
+    S = geo.rivers_by_station_number(stations, N)
+print(S)
+if __name__ == "__main__":
+    print("*** Task 1E: CUED Part IA Flood Warning System ***")
+    run()
