b2d2c403428199300b4a66833004e012fcb5ebd9
Reve Kragbe <rk681@cam.ac.uk>
2022-03-01 10:22:22 +0000

    task 2c

---
+++ task2c.py
@@ -0,0 +1,10 @@
+from floodsystem.flood import stations_highest_rel_level
+from floodsystem.stationdata import build_station_list, update_water_levels
+
+
+def run():
+    stations = build_station_list()
+    update_water_levels(stations)
+    high_risk_stations = stations_highest_rel_level(stations, 10)
+    for station in high_risk_stations:
+        print(station.name, station.relative_water_level())
